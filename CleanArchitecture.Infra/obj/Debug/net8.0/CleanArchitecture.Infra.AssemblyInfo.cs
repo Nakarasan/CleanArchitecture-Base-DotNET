383e4f33be28b59995e858ac2955c288478258e3
Nakarasan <krishzkara27@gmail.com>
2024-06-25 13:24:20 +0530

    changes

--- CleanArchitecture.Infra/obj/Debug/net8.0/CleanArchitecture.Infra.AssemblyInfo.cs
+++ CleanArchitecture.Infra/obj/Debug/net8.0/CleanArchitecture.Infra.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanArchitecture.Infra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03195e9c735e6790a5accd280bc0f1dcc6b68417")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanArchitecture.Infra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanArchitecture.Infra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
